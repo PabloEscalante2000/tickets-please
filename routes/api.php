e3170d0c7a084f4471f8fbd560ebab97ea7d2042
Pablo Escalante <pabloescalante200027@gmail.com>
2026-04-23 15:38:01 -0500

    primer commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,8 +9,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get("/",function(){
-    return response()->json([
-        "message" => "Welcome to the API"
-    ], 200);
+Route::post("/login", [AuthController::class, "login"]);
+
+Route::get("/tickets",function(){
+    return Ticket::all();
 });
