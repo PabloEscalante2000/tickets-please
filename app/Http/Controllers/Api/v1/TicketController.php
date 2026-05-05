1f0ea042b53501ad7c8430d3eb19cd42aadf3a76
Pablo Escalante <pabloescalante200027@gmail.com>
2026-05-05 17:43:13 -0500

    fah

--- app/Http/Controllers/Api/v1/TicketController.php
+++ app/Http/Controllers/Api/v1/TicketController.php
@@ -69,7 +69,24 @@ class TicketController extends ApiController
      */
     public function update(UpdateTicketRequest $request, $ticket_id)
     {
-        //
+        // PATCH
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+            $request->mappedAttributes();
+            $model = [
+                "title" => $request->input("data.attributes.title"),
+                "description" => $request->input("data.attributes.description"),
+                "status" => $request->input("data.attributes.status"),
+                "user_id" => $request->input("data.relationships.author.data.id")
+            ];
+
+            $ticket->update($model);
+
+            return new TicketResource($ticket);
+        } catch (ModelNotFoundException $th) {
+            return $this->error("Ticket cannot be found", 404);
+        }
     }
 
     public function replace(ReplaceTicketRequest $request, $ticket_id){
@@ -77,6 +94,7 @@ class TicketController extends ApiController
         try {
             $ticket = Ticket::findOrFail($ticket_id);
 
+            $request->mappedAttributes();
             $model = [
                 "title" => $request->input("data.attributes.title"),
                 "description" => $request->input("data.attributes.description"),
